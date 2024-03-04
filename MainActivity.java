c027810c52efc55b69a2953c9ea8bd47f4bb7c61
Ashwin Sebastian <56103423+ashwinsebastian76@users.noreply.github.com>
2024-03-05 10:13:44 +1100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -3,12 +3,20 @@ package com.example.week1_workshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int click=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        TextView text=(TextView) findViewById(R.id.textView);
+        click++;
+        text.setText("You have clicked "+click+" times.");
     }
 }
